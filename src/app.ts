f6be5245e8fe8806532043e960e455308978d4f1
Jyothis-Tency <jyothisgtency@gmail.com>
2025-02-25 16:29:46 +0530

    Test-5

--- src/app.ts
+++ src/app.ts
@@ -22,27 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 database_connection();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://next-way-frontend.vercel.app",
-  "https://next-way-frontend-fvstzp8b0-jyothis-tencys-projects.vercel.app",
-  /\.vercel\.app$/,
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.some((allowedOrigin) => origin.match(allowedOrigin))) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -52,11 +32,11 @@ app.use(morgan("dev"));
 app.use("/data/user", userRoutes);
 app.use("/data/company", companyRoutes);
 app.use("/data/admin", adminRoutes);
-app.use("/data/chat", chatRoutes);
-app.use("/data/subscribe", subscriptionRoutes);
+app.use("/data/chat", chatRoutes)
+app.use("/data/subscribe",subscriptionRoutes)
 
 app.use(errorHandler);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+})
